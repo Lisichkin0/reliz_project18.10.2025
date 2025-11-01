b27f0b708731a8d509bbd619cc157d8784605b7a
Lisichkin0 <melnykoleksandr2010@gmail.com>
2025-11-01 13:30:33 +0200

    day4

--- main.js
+++ main.js
@@ -8,16 +8,55 @@ async function getProducts() {
     return products
 };
 
-function getCardHTML(product){
+function getCardHTML(product) {
     return `<div class="card" style="width: 18rem;">
   <img src="img/${product.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.description}</p>
     <h5 class="card-title">${product.price}$</h5>
-    <a href="#" class="btn btn-primary">Buy</a>
+    <a href="#" class="btn btn-primary" data prodact='${JSON.stringify(product)}'>Buy</a>
   </div>
 </div>`
+}
+
+class ShoppingCart {
+    constructor() {
+        this.items = {} // об’єкт з товарами у кошику
+        this.total = 0  // загальна вартість замовлення
+    }
+
+    addItem(item) { // Додавання товару до кошика 
+        if (this.items[item.title]) {
+            this.items[item.title].quanity += 1;
+        }
+        else {
+            this.items[item.title] = item;
+            this.items[item.title].quanity = 1;
+        }
+        this.saveCartToCookies()
+    }
+
+    saveCartToCookies() { // збереження кошика у кукі
+        let cartJSON = JSON.stringify(this.items);
+        document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+    }
+
+    loadCartFromCookies() { // Завантаження кошика з кукі
+        let cartCookie = getCookieValue('cart');
+        if (cartCookie && cartCookie !== '') {
+            this.items = JSON.parse(cartCookie);
+        }
+    }
+}
+let cart = new ShoppingCart() // Створення об'єкта кошика
+
+function addToCart(event) {
+    // Отримуємо дані про товар з data-атрибута кнопки
+    const productData = event.target.getAttribute('data-product')
+    const product = JSON.parse(productData) // перетворюємо JSON у об’єкт
+    // Тут будемо додавати товар до кошика
+    cart.addItem(product)
 }
 
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
